b0b5fbbbcd7e1081d65bfaaa1ecaf25df6f7dcd4
HON CHOY <30066573@tafe.wa.edu.au>
2023-09-13 10:05:00 +0800

    添加项目文件。

---
+++ The super prototype of great Wiki 1.0/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+    namespace The_super_prototype_of_great_Wiki_1._0
+    {
+        public partial class Form1 : Form
+        {
+            private static readonly int rows = 12;
+            private static readonly int columns = 4;
+            private string[,] definitionsArray = new string[rows, columns];
+
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private int FindEmptyRow()
+            {
+                for (int row = 0; row < rows; row++)
+                {
+                    if (string.IsNullOrEmpty(definitionsArray[row, 0]))
+                    {
+                        return row;
+                    }
+                }
+                return -1; // No empty row found
+            }
+
+            private int FindRowIndexByName(string nameToFind)
+            {
+                for (int row = 0; row < rows; row++)
+                {
+                    if (definitionsArray[row, 0] == nameToFind)
+                    {
+                        return row;
+                    }
+                }
+                return -1; // Name not found
+            }
+
+            private void AddButton_Click(object sender, EventArgs e)
+            {
+                string name = NameTextBox.Text;
+                string category = CategoryTextBox.Text;
+                string description = DescriptionTextBox.Text;
+                string example = ExampleTextBox.Text;
+
+                // Find an empty slot in the array to store the definition
+                int rowIndex = FindEmptyRow();
+                if (rowIndex == -1)
+                {
+                    MessageBox.Show("No more space in the array to add a new definition.");
+                    return;
+                }
+
+                // Store the definition in the array
+                definitionsArray[rowIndex, 0] = name;
+                definitionsArray[rowIndex, 1] = category;
+                definitionsArray[rowIndex, 2] = description;
+                definitionsArray[rowIndex, 3] = example;
+
+                // Clear the input fields
+                NameTextBox.Text = "";
+                CategoryTextBox.Text = "";
+                DescriptionTextBox.Text = "";
+                ExampleTextBox.Text = "";
+
+                MessageBox.Show("Definition added successfully.");
+            }
+
+            private void EditButton_Click(object sender, EventArgs e)
+            {
+                string nameToEdit = NameTextBox.Text;
+
+                // Find the index of the definition with the matching Name
+                int rowIndex = FindRowIndexByName(nameToEdit);
+
+                if (rowIndex == -1)
+                {
+                    MessageBox.Show("Definition not found.");
+                    return;
+                }
+
+                string newCategory = CategoryTextBox.Text;
+                string newDescription = DescriptionTextBox.Text;
+                string newExample = ExampleTextBox.Text;
+
+                // Update the definition in the array
+                definitionsArray[rowIndex, 1] = newCategory;
+                definitionsArray[rowIndex, 2] = newDescription;
+                definitionsArray[rowIndex, 3] = newExample;
+
+                // Clear the input fields
+                NameTextBox.Text = "";
+                CategoryTextBox.Text = "";
+                DescriptionTextBox.Text = "";
+                ExampleTextBox.Text = "";
+
+                MessageBox.Show("Definition updated successfully.");
+            }
+
+            private void DeleteButton_Click(object sender, EventArgs e)
+            {
+                string nameToDelete = NameTextBox.Text;
+
+                for (int row = 0; row < rows; row++)
+                {
+                    if (definitionsArray[row, 0] == nameToDelete)
+                    {
+                        // Delete the definition by setting all columns to null
+                        for (int col = 0; col < columns; col++)
+                        {
+                            definitionsArray[row, col] = null;
+                        }
+
+                        // Clear the input fields
+                        NameTextBox.Text = "";
+                        CategoryTextBox.Text = "";
+                        DescriptionTextBox.Text = "";
+                        ExampleTextBox.Text = "";
+
+                        MessageBox.Show("Definition deleted successfully.");
+                        return;
+                    }
+                }
+
+                MessageBox.Show("Definition not found.");
+            }
+
+            private void ClearButton_Click(object sender, EventArgs e)
+            {
+                // Clear the input fields
+                NameTextBox.Text = "";
+                CategoryTextBox.Text = "";
+                DescriptionTextBox.Text = "";
+                ExampleTextBox.Text = "";
+            }
+
+            private void SortButton_Click(object sender, EventArgs e)
+            {
+                // Implement a bubble sort algorithm to sort the array by Name
+                // (Similar to the code provided earlier in the console application)
+                // Make sure to update the display after sorting if necessary.
+            }
+
+            private void SearchButton_Click(object sender, EventArgs e)
+            {
+                string nameToSearch = NameTextBox.Text;
+
+                int rowIndex = FindRowIndexByName(nameToSearch);
+
+                if (rowIndex != -1)
+                {
+                    // Display the found definition in the input fields
+                    CategoryTextBox.Text = definitionsArray[rowIndex, 1];
+                    DescriptionTextBox.Text = definitionsArray[rowIndex, 2];
+                    ExampleTextBox.Text = definitionsArray[rowIndex, 3];
+                }
+                else
+                {
+                    // Clear the input fields
+                    CategoryTextBox.Text = "";
+                    DescriptionTextBox.Text = "";
+                    ExampleTextBox.Text = "";
+
+                    MessageBox.Show("Definition not found.");
+                }
+            }
+
+            private void DisplayButton_Click(object sender, EventArgs e)
+            {
+                // Implement the logic to display definitions in a list view or other control
+            }
+
+            private void LoadButton_Click(object sender, EventArgs e)
+            {
+                // Implement the logic to load definitions from a file into the definitionsArray
+                // (Similar to the code provided earlier in the console application)
+            }
+
+            private void SaveButton_Click(object sender, EventArgs e)
+            {
+                // Implement the logic to save definitions from the definitionsArray into a file
+                // (Similar to the code provided earlier in the console application)
+            }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(889, 458);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+      }
+    }
---
+++ The super prototype of great Wiki 1.0/Form1Helpers.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+internal static class Form1Helpers
+{
+    private static readonly int rows = 12;
+    private static readonly int columns = 4;
+    private static string[,] definitionsArray = new string[rows, columns];
+
+    // 9.1: Create a global 2D string array
+    // DefinitionsArray is already declared above.
+
+    // 9.2: Create an ADD button that will store the information from the 4 text boxes into the 2D array
+    static void AddDefinition()
+    {
+        // Implement adding a definition to the array
+    }
+
+    // 9.5: Create a CLEAR method to clear the four text boxes so a new definition can be added
+    static void ClearFields()
+    {
+        // Implement clearing the input fields
+    }
+
+    // 9.4: Create a DELETE button that removes all the information from a single entry of the array
+    static void DeleteDefinition()
+    {
+        // Implement deleting a definition from the array
+    }
+
+    // 9.8: Create a display method that will show Name and Category in a ListView
+    static void DisplayDefinitions()
+    {
+        // Implement displaying definitions in a ListView
+    }
+
+    // 9.3: Create an EDIT button that will allow the user to modify any information from the 4 text boxes into the 2D array
+    static void EditDefinition()
+    {
+        // Implement editing a definition in the array
+    }
+
+    // 9.11: Create a LOAD button that will read the information from a binary file into the 2D array
+    static void LoadDefinitionsFromFile()
+    {
+        // Implement loading definitions from a binary file
+    }
+
+    static void Main(string[] args)
+    {
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.WriteLine("Dictionary Management App");
+            Console.WriteLine("1. Add Definition");
+            Console.WriteLine("2. Edit Definition");
+            Console.WriteLine("3. Delete Definition");
+            Console.WriteLine("4. Clear Fields");
+            Console.WriteLine("5. Sort Definitions");
+            Console.WriteLine("6. Search Definition");
+            Console.WriteLine("7. Display Definitions");
+            Console.WriteLine("8. Load Definitions from File");
+            Console.WriteLine("9. Save Definitions to File");
+            Console.WriteLine("10. Exit");
+            Console.Write("Select an option (1-10): ");
+
+            int choice;
+            if (int.TryParse(Console.ReadLine(), out choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        AddDefinition();
+                        break;
+                    case 2:
+                        EditDefinition();
+                        break;
+                    case 3:
+                        DeleteDefinition();
+                        break;
+                    case 4:
+                        ClearFields();
+                        break;
+                    case 5:
+                        SortDefinitions();
+                        break;
+                    case 6:
+                        SearchDefinition();
+                        break;
+                    case 7:
+                        DisplayDefinitions();
+                        break;
+                    case 8:
+                        LoadDefinitionsFromFile();
+                        break;
+                    case 9:
+                        SaveDefinitionsToFile();
+                        break;
+                    case 10:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+    }
+
+    // 9.10: Create a SAVE button so the information from the 2D array can be written into a binary file
+    static void SaveDefinitionsToFile()
+    {
+        // Implement saving definitions to a binary file
+    }
+
+    // 9.7: Write the code for a Binary Search for the Name in the 2D array
+    static void SearchDefinition()
+    {
+        // Implement binary search to find a definition in the array
+    }
+
+    // 9.9: Create a method so the user can select a definition (Name) from the ListView and all the information is displayed
+    static void SelectDefinitionFromListView()
+    {
+        // Implement selecting a definition from the ListView
+    }
+
+    // 9.6: Write the code for a Bubble Sort method to sort the 2D array by Name ascending
+    static void SortDefinitions()
+    {
+        // Implement a bubble sort algorithm to sort the array
+    }
+}
